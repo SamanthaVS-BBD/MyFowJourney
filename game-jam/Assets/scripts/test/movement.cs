889bf988cdfc3a9e07b109a498130e8b13719264
Joseph-Kimathi <joseph.kimathi@bbd.co.za>
2024-08-24 22:43:06 +0200

    Update movement.cs

--- game-jam/Assets/scripts/test/movement.cs
+++ game-jam/Assets/scripts/test/movement.cs
@@ -155,7 +155,10 @@ public class movement : MonoBehaviour
     void Jump()
     {
         Debug.Log("WE ARE JUMPING");
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        if (rb.rotation > 10)
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce * 2.2f);
+        else
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
 
         isJumping = true;
     }
